4f95c09b171d2d87368c7dcc9fafc4c895b5ec47
Nicholas Oyler <noyler24@southhills.edu>
2010-09-09 16:04:08 -0400

    Declared Variables

--- FrmMain.vb
+++ FrmMain.vb
@@ -1,3 +1,16 @@
 ﻿Public Class FrmMain
 
+    Private Sub btnDisplayAccountSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAccountSummary.Click
+
+        Dim readerVar As IO.StreamReader = IO.File.OpenText("Data.txt")
+        Dim strName As String = ""
+        Dim strAccountNumber As String = ""
+        Dim dblPastDue As Double = 0
+        Dim dblPayments As Double = 0
+        Dim dblPurchases As Double = 0
+        Dim dblFinanceChargers As Double = (dblPastDue - dblPayments) / 1.5 / 100
+        Dim dblCurrentBalance As Double = dblPastDue - dblPayments + dblPurchases + dblFinanceChargers
+
+
+    End Sub
 End Class
