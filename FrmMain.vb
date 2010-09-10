5fd2ea468a1ba0228e9f57b9ec6a8412704ea173
Nicholas Oyler <noyler24@southhills.edu>
2010-09-10 15:41:42 -0400

    Added First Readline and Listbox Output

--- FrmMain.vb
+++ FrmMain.vb
@@ -2,7 +2,7 @@
 
     Private Sub btnDisplayAccountSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAccountSummary.Click
 
-        Dim readerVar As IO.StreamReader = IO.File.OpenText("Data.txt")
+        Dim sr As IO.StreamReader = IO.File.OpenText("..\..\Data.txt")
         Dim strName As String = ""
         Dim strAccountNumber As String = ""
         Dim dblPastDue As Double = 0
@@ -10,6 +10,17 @@
         Dim dblPurchases As Double = 0
         Dim dblFinanceChargers As Double = (dblPastDue - dblPayments) / 1.5 / 100
         Dim dblCurrentBalance As Double = dblPastDue - dblPayments + dblPurchases + dblFinanceChargers
+
+        strName = sr.ReadLine
+        dblPastDue = sr.ReadLine
+        dblPayments = sr.ReadLine
+        dblPurchases = sr.ReadLine
+        dblFinanceChargers = sr.ReadLine
+        dblCurrentBalance = sr.ReadLine
+
+        lstAccountSummary.Items.Add(strName & dblPastDue & dblPayments & dblPurchases & dblFinanceChargers & dblCurrentBalance)
+
+
 
 
     End Sub
