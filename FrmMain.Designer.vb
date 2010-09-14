c942568af46e34f4fcf17b867a0fe4828c841e8d
Nicholas Oyler <noyler24@southhills.edu>
2010-09-14 14:54:42 -0400

    Finished Program and added comments

--- FrmMain.Designer.vb
+++ FrmMain.Designer.vb
@@ -28,9 +28,9 @@ Partial Class FrmMain
         '
         'btnDisplayAccountSummary
         '
-        Me.btnDisplayAccountSummary.Location = New System.Drawing.Point(136, 45)
+        Me.btnDisplayAccountSummary.Location = New System.Drawing.Point(159, 48)
         Me.btnDisplayAccountSummary.Name = "btnDisplayAccountSummary"
-        Me.btnDisplayAccountSummary.Size = New System.Drawing.Size(328, 72)
+        Me.btnDisplayAccountSummary.Size = New System.Drawing.Size(383, 78)
         Me.btnDisplayAccountSummary.TabIndex = 0
         Me.btnDisplayAccountSummary.Text = "Display Account Summary"
         Me.btnDisplayAccountSummary.UseVisualStyleBackColor = True
@@ -38,18 +38,20 @@ Partial Class FrmMain
         'lstAccountSummary
         '
         Me.lstAccountSummary.FormattingEnabled = True
-        Me.lstAccountSummary.Location = New System.Drawing.Point(12, 158)
+        Me.lstAccountSummary.ItemHeight = 14
+        Me.lstAccountSummary.Location = New System.Drawing.Point(14, 170)
         Me.lstAccountSummary.Name = "lstAccountSummary"
-        Me.lstAccountSummary.Size = New System.Drawing.Size(578, 160)
+        Me.lstAccountSummary.Size = New System.Drawing.Size(674, 172)
         Me.lstAccountSummary.TabIndex = 1
         '
         'FrmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(614, 374)
+        Me.ClientSize = New System.Drawing.Size(716, 403)
         Me.Controls.Add(Me.lstAccountSummary)
         Me.Controls.Add(Me.btnDisplayAccountSummary)
+        Me.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "FrmMain"
         Me.Text = "Credit Card Accounts"
         Me.ResumeLayout(False)
--- FrmMain.vb
+++ FrmMain.vb
@@ -2,27 +2,31 @@
 
     Private Sub btnDisplayAccountSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAccountSummary.Click
 
+        'Declare Variables
         Dim sr As IO.StreamReader = IO.File.OpenText("..\..\Data.txt")
         Dim strName As String = ""
         Dim strAccountNumber As String = ""
         Dim dblPastDue As Double = 0
         Dim dblPayments As Double = 0
         Dim dblPurchases As Double = 0
-        Dim fmtStr As String = "{0,-15}{1,15}{2,15}{3,15}{4,15}{5,15}"
-        
+        Dim fmtStr As String = "{0,-10}{1,15}{2,15}{3,15}{4,15}{5,15}"
 
+        'Read text Document
         strName = sr.ReadLine
         dblPastDue = sr.ReadLine
         dblPayments = sr.ReadLine
         dblPurchases = sr.ReadLine
 
+        'Delare Variables and Process Calculations
         Dim dblNewBalance As Double = (dblPastDue - dblPayments)
         Dim dblFinanceChargers As Double = (dblNewBalance * 1.015) - dblNewBalance
         Dim dblCurrentBalance As Double = (dblNewBalance + dblPurchases + dblFinanceChargers)
 
+        'Output
         lstAccountSummary.Items.Clear()
         lstAccountSummary.Items.Add(String.Format(fmtStr, "Account", "Past Due", "", "", "Finance", "Current"))
-        'lstAccountSummary.Items.Add(strName & FormatCurrency(dblPastDue, 2) & FormatCurrency(dblPayments, 2) & FormatCurrency(dblPurchases, 2) & FormatCurrency(dblFinanceChargers, 2) & FormatCurrency(dblCurrentBalance, 2))
+        lstAccountSummary.Items.Add(String.Format(fmtStr, "Number", "Amount", "Payments", "Purchases", "Changes", "Amt Due"))
+        lstAccountSummary.Items.Add(String.Format(fmtStr, strName, FormatCurrency(dblPastDue, 2), FormatCurrency(dblPayments, 2), FormatCurrency(dblPurchases, 2), FormatCurrency(dblFinanceChargers, 2), FormatCurrency(dblCurrentBalance, 2)))
 
     End Sub
 End Class
