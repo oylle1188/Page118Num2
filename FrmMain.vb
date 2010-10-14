82bb0a59820a2dd35621ab20df46791c3719a4d6
Nicholas Oyler <noyler24@southhills.edu>
2010-10-14 16:20:27 -0400

    Edited Code. Fixed the Program

--- FrmMain.vb
+++ FrmMain.vb
@@ -11,22 +11,30 @@
         Dim dblPurchases As Double = 0
         Dim fmtStr As String = "{0,-10}{1,15}{2,15}{3,15}{4,15}{5,15}"
 
-        'Read text Document
-        strName = sr.ReadLine
-        dblPastDue = sr.ReadLine
-        dblPayments = sr.ReadLine
-        dblPurchases = sr.ReadLine
-
-        'Delare Variables and Process Calculations
-        Dim dblNewBalance As Double = (dblPastDue - dblPayments)
-        Dim dblFinanceChargers As Double = (dblNewBalance * 1.015) - dblNewBalance
-        Dim dblCurrentBalance As Double = (dblNewBalance + dblPurchases + dblFinanceChargers)
-
-        'Output
-        lstAccountSummary.Items.Clear()
         lstAccountSummary.Items.Add(String.Format(fmtStr, "Account", "Past Due", "", "", "Finance", "Current"))
         lstAccountSummary.Items.Add(String.Format(fmtStr, "Number", "Amount", "Payments", "Purchases", "Changes", "Amt Due"))
-        lstAccountSummary.Items.Add(String.Format(fmtStr, strName, FormatCurrency(dblPastDue, 2), FormatCurrency(dblPayments, 2), FormatCurrency(dblPurchases, 2), FormatCurrency(dblFinanceChargers, 2), FormatCurrency(dblCurrentBalance, 2)))
+
+        'Read text Document
+        While Not sr.EndOfStream
+            strName = sr.ReadLine
+            dblPastDue = sr.ReadLine
+            dblPayments = sr.ReadLine
+            dblPurchases = sr.ReadLine
+
+            'Delare Variables and Process Calculations
+            Dim dblNewBalance As Double = (dblPastDue - dblPayments)
+            Dim dblFinanceChargers As Double = (dblNewBalance * 1.015) - dblNewBalance
+            Dim dblCurrentBalance As Double = (dblNewBalance + dblPurchases + dblFinanceChargers)
+
+            lstAccountSummary.Items.Add(String.Format(fmtStr, strName, FormatCurrency(dblPastDue, 2), FormatCurrency(dblPayments), FormatCurrency(dblPurchases), FormatCurrency(dblFinanceChargers), FormatCurrency(dblCurrentBalance)))
+        End While
+      
+
+
+    
+
+       
+
 
     End Sub
 End Class
